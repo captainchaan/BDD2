3ccde67a489defce68e5b5595b08a143d8c925c7
TEST <annivanova@list.ru>
2024-12-21 18:59:25 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/df6xywtlp2mwmea5/branch/main?svg=true)](https://ci.appveyor.com/project/captainchaan/bdd/branch/main)
---
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -0,0 +1,44 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private SelenideElement header = $("[data-test-id=dashboard] ~h1");
+
+    public TransferPage() {
+        header.shouldBe(visible).shouldHave(Condition.text("Пополнение карты"));
+    }
+
+    private SelenideElement amountField = $("[data-test-id=amount] .input__control");
+    private SelenideElement numberCardField = $("[data-test-id=from] .input__control");
+    private SelenideElement buttonTransfer = $("[data-test-id=action-transfer]");
+    private SelenideElement buttonCancel = $("[data-test-id=action-cancel]");
+
+    public void fillInForm(Integer sum, DataHelper.CardInfo card) {
+        amountField.sendKeys(Keys.chord(Keys.CONTROL + "A"), Keys.DELETE);
+        amountField.setValue(Integer.toString(sum));
+        numberCardField.sendKeys(Keys.chord(Keys.CONTROL + "A"), Keys.DELETE);
+        numberCardField.sendKeys(card.getCardNumber());
+    }
+
+    public DashboardPage replCardBalance(Integer sum, DataHelper.CardInfo card) {
+        fillInForm(sum, card);
+        buttonTransfer.click();
+        return new DashboardPage();
+    }
+
+    public DashboardPage replCardCancel(Integer sum, DataHelper.CardInfo card) {
+        fillInForm(sum, card);
+        buttonCancel.click();
+        return new DashboardPage();
+    }
+}
